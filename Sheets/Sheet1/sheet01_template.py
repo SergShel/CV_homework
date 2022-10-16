119a76b72a686a23f7c7b08df7fcff4ae1426410
SergShel <43682508+SergShel@users.noreply.github.com>
2022-10-16 20:11:36 +0200

    Sheet01 exercise upload

---
+++ Sheets/Sheet1/sheet01_template.py
@@ -0,0 +1,60 @@
+import cv2 as cv
+import numpy as np
+import random
+import time
+
+# ********************TASK1***********************
+def integral_image(img):
+    # Your implementation of integral image
+    pass
+
+
+def sum_image(image):
+    # Your implementation for summing up pixels
+    pass
+
+
+def task1():
+    # Your implementation of Task1
+    pass
+# ************************************************
+# ********************TASK2***********************
+def equalize_hist_image(img):
+    # Your implementation of histogram equalization
+    pass
+
+
+def task2():
+    # Your implementation of Task2
+    pass
+# ************************************************
+# ********************TASK4***********************
+def get_kernel(sigma):
+    # Your implementation of getGaussianKernel
+    pass
+
+
+def task4():
+    # Your implementation of Task4
+    pass
+# ************************************************
+# ********************TASK5***********************
+def task5():
+    # Your implementation of Task5
+    pass
+# ************************************************
+# ********************TASK7***********************
+def add_salt_n_pepper_noise(img):
+    # Your implementation of adding noise to the image
+    pass
+
+
+def task7():
+    # Your implementation of task 7
+    pass
+
+# ************************************************
+# ********************TASK8***********************
+def task8():
+    # Your implementation of task 8
+    pass

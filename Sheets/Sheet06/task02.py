c7c42c3d09d31334b2f6aa5ed5720ad99567dd48
SergShel <43682508+SergShel@users.noreply.github.com>
2022-12-06 22:22:45 +0100

    Sheet 6. Task2 finish

--- Sheets/Sheet06/task02.py
+++ Sheets/Sheet06/task02.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-observations = np.load('data/observations.npy')
+observations = np.load('Sheet06\data\observations.npy')
 
 
 def get_observation(t):
@@ -25,17 +25,31 @@ class KalmanFilter(object):
         self.tau = tau
 
     def init(self, init_state):
-        # self.state =
-        # self.covariance =
-        pass
+        self.state = init_state
+        self.covariance = np.identity(init_state.shape[0])
 
     def track(self, xt):
-        # to do
-        pass
+        
+        # State Prediction.  self.state is the previous state (mu_t_minus_1)
+        mu_plus = np.matmul(self.psi, self.state)
+
+        # Covariance Prediction.  self.covariance is the previous covariance (Sigma_t_minus_1)
+        sigma_plus = self.sigma_p + np.matmul(np.matmul(self.psi, self.covariance), self.psi.T) 
+
+        # Kalman Gain.  
+        K = np.matmul(np.matmul(sigma_plus, self.phi.T), np.linalg.inv(self.sigma_m + np.matmul(np.matmul(self.phi, sigma_plus), self.phi.T)))
+
+        # State Update.  xt is the current observation 
+        self.state = mu_plus + np.matmul(K, (xt - np.matmul(self.phi, mu_plus)))   # TODO mu_m ????
+
+        # Covariance Update.
+        self.covariance = np.matmul((np.identity(self.state.shape[0]) - np.matmul(K, self.phi)), sigma_plus)  
+
 
     def get_current_location(self):
-        # to do
-        pass
+        curr_location = np.matmul(self.phi, self.state)
+        return curr_location
+
 
 def perform_tracking(tracker):
     track = []
@@ -67,7 +81,13 @@ def get_world_model():
 def main():
 
     psi, sigma_p, phi, sigma_m = get_world_model()
+    
+    init_state = np.array([-10, -15, 1, -2])
 
+    kalman_filter = KalmanFilter(psi, sigma_p, phi, sigma_m, 0.1)
+    kalman_filter.init(init_state)
+
+    track = perform_tracking(kalman_filter)
 
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
@@ -76,9 +96,7 @@ def main():
     for n in range(num_steps):
         ax.clear()
         o_x, o_y = get_data_at_iteration(n, observations) 
-        # TODO: 
-        #t_x, t_y = get_data_at_iteration(n, track) 
-        t_x, t_y = 0, 0
+        t_x, t_y = get_data_at_iteration(n, track) 
          
         ax.plot(o_x, o_y, 'g', label='observations')
         ax.plot(t_x, t_y, 'y', label='Kalman')
